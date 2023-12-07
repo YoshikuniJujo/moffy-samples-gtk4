0ee25ec76232ac6a90d0ddcb713b3c3e2bab3022
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-07 13:35:52 +0900

    add source codes

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
 module Main (main) where
 
-import Lib
+import System.Environment
+import Control.Moffy.Samples.Boxes
+import Control.Moffy.Samples.Boxes.Run.Gtk4
+import Control.Moffy.Samples.Followbox
+import Control.Moffy.Samples.Followbox.Run.Gtk4
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= \case
+	["boxes"] -> runBoxes boxes
+	["followbox"] -> runFollowbox "firefox" followbox
+	_ -> putStrLn $
+		"Usage: moffy_samples [SUBCOMMAND]\n\n" ++
+		"Commands are\n\tboxes\n\tfollowbox"
