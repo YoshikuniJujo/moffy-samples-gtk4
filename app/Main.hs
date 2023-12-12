a192fb6af7a5b3d6b94b18eb2a1e213f2f0b3310
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2023-12-12 15:40:44 +0900

    followbox-another and followbox-origin

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
@@ -7,12 +8,17 @@ import System.Environment
 import Control.Moffy.Samples.Boxes
 import Control.Moffy.Samples.Boxes.Run.Gtk4
 import Control.Moffy.Samples.Followbox
+import Control.Moffy.Samples.FollowboxAnother qualified as A
+import Control.Moffy.Samples.FollowboxOrigin qualified as O
 import Control.Moffy.Samples.Followbox.Run.Gtk4
 
 main :: IO ()
 main = getArgs >>= \case
 	["boxes"] -> runBoxes boxes
 	["followbox"] -> runFollowbox "firefox" followbox
+	["followbox-another"] -> runFollowbox' "firefox" A.followbox
+	["followbox-origin"] -> runFollowbox "firefox" O.followbox
 	_ -> putStrLn $
 		"Usage: moffy_samples [SUBCOMMAND]\n\n" ++
-		"Commands are\n\tboxes\n\tfollowbox"
+		"Commands are\n\tboxes\n" ++
+		"\tfollowbox\n\tfollowbox-another\n\tfollowbox-origin"
